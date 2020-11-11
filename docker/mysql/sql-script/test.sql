fa3efefcceed76ae1474792d080ddea4bac975cb
lenormandSeb <lenormand.sbn@gmail.com>
2020-11-11 19:40:23 +0100

    Feat/docker mysql (#2)
    * Init commit for mysql
    
    * databaseDocker_yorik
    
    * Init commit for mysql
    
    * databaseDocker_yorik
    
    * Chore(clean) branch
    
    * "merge yorik"
    
    * Init commit for mysql
    
    * databaseDocker_yorik
    
    * Chore(clean) branch
    
    * fix node_modules
    
    * maj script sql
    
    Co-authored-by: moreau <yorik-ba-56@hotmail.Fr>

---
+++ docker/mysql/sql-script/test.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 11 nov. 2020 à 19:08
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `test`
+--
+
+CREATE DATABASE IF NOT EXISTS security_bdd
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `test`
+--
+
+CREATE TABLE IF NOT EXISTS `test` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `prenom` varchar(600) NOT NULL,
+  `dateNaissance` date NOT NULL,
+  `adresseMail` varchar(250) NOT NULL,
+  `tel` int(11) NOT NULL,
+  `adressePostale` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
